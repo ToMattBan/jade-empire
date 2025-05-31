e633f594824273550e31c9772bed9b5fa732a2ab
ToMattBan <souaquelecleyton@yandex.com>
2025-05-30 21:37:25 -0300

    Using real XML

--- app.vue
+++ app.vue
@@ -83,8 +83,8 @@ async function getXML() {
   const fileInfo = await githubFileInfo.json();
   fileSha.value = fileInfo.sha;
 
-  //const fileRawURL = fileInfo.download_url;
-  const fileRawURL = '/api/mockXml';
+  const fileRawURL = fileInfo.download_url;
+  //const fileRawURL = '/api/mockXml';
 
   const githubXML = await fetch(fileRawURL);
   let xmlRaw = await githubXML.text();
